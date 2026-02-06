a203d96408b525a77e9bbc251ab5346bde9fd85d
Cristian Marinescu <cristian.marinescu92@yahoo.com>
2026-02-06 15:40:09 +0100

    create header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+import Header from "@/components/ui/Header";
 
 export default function Home() {
   return (
-    
+    <>
+    <Header />
+    <main>
+
+    </main>
+    </>
   );
 }
